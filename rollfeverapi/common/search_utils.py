401970a7eaf51ab6144ae839319d06e005237951
Carlos Correia <carlos.correia@ongagement.com>
2016-05-27 18:30:20 +0200

    uppercase-lowercase search

--- rollfeverapi/common/search_utils.py
+++ rollfeverapi/common/search_utils.py
@@ -6,7 +6,7 @@ def generic_search():
     print('search')
 
 def user_search(query):
-    users_profiles = Profile.objects.filter(Q(account__username__startswith=query) | Q(account__email=query) | Q(name__startswith=query)).values_list('account')
+    users_profiles = Profile.objects.filter(Q(account__username__istartswith=query) | Q(account__email=query) | Q(name__istartswith=query)).values_list('account')
     users = MyUser.objects.filter(id__in = users_profiles)
     serializer = UserHeadingSerializer(users, many=True)
     return serializer.data
