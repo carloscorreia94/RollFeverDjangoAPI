013eb310d6a2e48a04d94194d2df6e0c54eebed4
Carlos Correia <carlos.correia@ongagement.com>
2016-06-21 16:27:47 +0200

    continue media

--- core_features/models.py
+++ core_features/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 
 class PendingMedia(models.Model):
-    media_number = models.IntegerField()
+    pending_media_number = models.IntegerField()
     media_type = models.CharField(max_length=100)
     content_id = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
@@ -12,7 +12,7 @@ class PendingMedia(models.Model):
     @staticmethod
     def add_pending_media(media_number, media_type, content_id):
         pending_media = PendingMedia()
-        pending_media.media_number = media_number
+        pending_media.pending_media_number = media_number
         pending_media.media_type = media_type
         pending_media.content_id = content_id
         pending_media.save()
@@ -20,5 +20,5 @@ class PendingMedia(models.Model):
 
     @staticmethod
     def media_to_wait(media_type):
-        #TODO: FILTER OBJECTS WITH PENDING MEDIA MORE THAN 0!!!!
-        return PendingMedia.objects.filter(media_type=media_type).values_list('content_id')
+        return PendingMedia.objects.filter(media_type=media_type, pending_media_number__gt=0).values_list('content_id')
+    
