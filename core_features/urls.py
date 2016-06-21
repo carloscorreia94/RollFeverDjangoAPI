5bb20b2d4b1b627e5602d3be4901c60d67556dc2
Carlos Correia <carlos.correia@ongagement.com>
2016-06-21 11:54:04 +0200

    started core_features. media

--- core_features/urls.py
+++ core_features/urls.py
@@ -3,6 +3,4 @@ from . import views
 
 urlpatterns = [
     url(r'^media/upload/(?P<media_type>\w+)/(?P<content_id>[0-9]+)/$', views.UploadMedia.as_view(), name="upload_media"),
-    url(r'^login/$', views.Login.as_view(), name="login"),
-    url(r'^refresh/$', views.Refresh.as_view(), name="refresh"),
 ]
