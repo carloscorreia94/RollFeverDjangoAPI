d46f4b8d0cce425a720370bf2826c3dfee65642d
Carlos Correia <carlos.correia@ongagement.com>
2016-07-07 17:11:45 +0200

    refactor name

--- spotmaniaapi/urls.py
+++ spotmaniaapi/urls.py
@@ -1,4 +1,4 @@
-"""rollfeverapi URL Configuration
+"""spotmaniaapi URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.8/topics/http/urls/
