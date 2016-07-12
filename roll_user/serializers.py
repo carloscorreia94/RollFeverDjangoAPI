9ee0a165c05d941ad041459d357d8b3f5d44be2a
Carlos Correia <carlos.correia@ongagement.com>
2016-07-12 12:46:16 +0200

    added user_connections to profile

--- roll_user/serializers.py
+++ roll_user/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from rest_auth.models import MyUser, Profile
-from .models import FollowerRelation
+from roll_user.models import FollowerRelation
 from rest_auth.serializers import UserProfileSerializer
 from spotmaniaapi import settings
 
