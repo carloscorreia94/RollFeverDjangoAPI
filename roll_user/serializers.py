6c5da435ac4569a19f0f62cfa1c4bb773d5d1c60
Carlos Correia <carlos.correia@ongagement.com>
2016-07-12 12:42:58 +0200

    added user_connections to profile

--- roll_user/serializers.py
+++ roll_user/serializers.py
@@ -1,10 +1,12 @@
 from rest_framework import serializers
 from rest_auth.models import MyUser, Profile
+from .models import FollowerRelation
 from rest_auth.serializers import UserProfileSerializer
 from spotmaniaapi import settings
 
 class UserHeadingSerializer(serializers.ModelSerializer):
     profile = serializers.SerializerMethodField('profile_heading')
+    numbers = serializers.SerializerMethodField('profile_numbers')
 
     def profile_heading(self,user):
         profile = Profile.objects.get(account=user)
@@ -13,6 +15,12 @@ class UserHeadingSerializer(serializers.ModelSerializer):
             'user_photo' : settings.MEDIA_URL + str(profile.user_photo) if str(profile.user_photo) != "" else None
         }
 
+    def profile_numbers(self,user):
+        user_connections = FollowerRelation.get_follow_status(user)
+        return {
+            'user_connections' : user_connections
+        }
+
     class Meta:
         model = MyUser
-        fields = ('username','profile',)
+        fields = ('username','profile','numbers')
