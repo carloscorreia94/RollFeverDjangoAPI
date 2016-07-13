75d285f6c5087b3f14ab5864ff2b556823ecdfd5
Carlos Correia <carlos.correia@ongagement.com>
2016-07-13 23:30:17 +0200

    developing websiteMonitor

--- core_features/tasks.py
+++ core_features/tasks.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import
 from celery import shared_task
 from .models import WebsiteSimpleMonitor
 import requests
-import BeautifulSoup
+from bs4 import BeautifulSoup
 from django.db.models import ObjectDoesNotExist
 
 
