056fb66ab6a0dc5f8a9e193139710a44b6c50b45
Carlos Correia <carlos.correia@ongagement.com>
2016-07-13 22:45:30 +0200

    developing websiteMonitor

--- core_features/tasks.py
+++ core_features/tasks.py
@@ -1,7 +1,10 @@
 from __future__ import absolute_import
 
 from celery import shared_task
+from .models import WebsiteSimpleMonitor
 import requests
+from django.db.models import ObjectDoesNotExist
+
 
 website = 'http://mobile.aeist.pt/teste_changes.html'
 element = 'mutant_div'
@@ -10,6 +13,14 @@ element = 'mutant_div'
 def check_webpage():
 
     r = requests.get(website)
+
+    try:
+        current_image = WebsiteSimpleMonitor.object.get(website_url=website)
+    except ObjectDoesNotExist:
+        new_image = WebsiteSimpleMonitor()
+        new_image.website_url = website
+        new_image.website_content_div = r.text
+
     print(str(r))
 
     print("hmmmm worka")
