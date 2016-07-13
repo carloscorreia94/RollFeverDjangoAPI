f828075c34bc28389ade500ee2abac4197293f3a
Carlos Correia <carlos.correia@ongagement.com>
2016-07-13 21:59:24 +0200

    updated task for html_changes verifier

--- core_features/tasks.py
+++ core_features/tasks.py
@@ -1,9 +1,25 @@
 from __future__ import absolute_import
 
 from celery import shared_task
+import requests
+
+  """
+    TODO: HTTP REQUEST
+            FIND ELEMENT- SAVE DIV CONTENTS DATABASE
+
+
+  """
+website = 'http://mobile.aeist.pt/teste_changes.html'
+element = 'mutant_div'
 
 @shared_task
 def check_webpage():
+
+    r = requests.get(website)
+    print(str(r))
+
     print("hmmmm worka")
     param = "um PARAM"
     return 'The test task executed with argument "%s" ' % param
+
+
