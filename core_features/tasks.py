ed7f49f7290583056cb381a1f75ced09b38c04a4
Carlos Correia <carlos.correia@ongagement.com>
2016-07-13 23:12:35 +0200

    developing websiteMonitor

--- core_features/tasks.py
+++ core_features/tasks.py
@@ -8,18 +8,24 @@ from django.db.models import ObjectDoesNotExist
 
 website = 'http://mobile.aeist.pt/teste_changes.html'
 element = 'mutant_div'
+id_or_class = 'class'
 
 @shared_task
 def check_webpage():
 
     r = requests.get(website)
+    look_for = "<div %s=\"%s\">" & (id_or_class,element)
+
+    div_content = r.text.split(look_for)[1]
+
+    #LETS FIND THE MUTANT DIV IN THE HTML CONTENT STRING
 
     try:
         current_image = WebsiteSimpleMonitor.objects.get(website_url=website)
     except ObjectDoesNotExist:
         new_image = WebsiteSimpleMonitor()
         new_image.website_url = website
-        new_image.website_content_div = r.text
+        new_image.website_content_div = div_content
         new_image.save()
 
     print(str(r))
