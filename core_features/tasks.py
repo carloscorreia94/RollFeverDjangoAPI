52d45a9140e0abc32ab13560b766671b429a2d30
Carlos Correia <carlos.correia@ongagement.com>
2016-07-13 20:45:06 +0200

    added celery to project dependences

--- core_features/tasks.py
+++ core_features/tasks.py
@@ -3,5 +3,6 @@ from __future__ import absolute_import
 from celery import shared_task
 
 @shared_task
-def test(param):
+def check_webpage(param):
+    print("hmmmm worka")
     return 'The test task executed with argument "%s" ' % param
