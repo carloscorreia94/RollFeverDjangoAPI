909af424ee573482d7e52e0697b70872c3780813
Carlos Correia <carlos.correia@ongagement.com>
2016-07-14 02:21:12 +0200

    developing websiteMonitor

--- core_features/tasks.py
+++ core_features/tasks.py
@@ -14,7 +14,8 @@ id_or_class = 'class'
 @shared_task
 def check_webpage():
 
-    r = requests.get(website)
+    headers = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_1) AppleWebKit/601.2.7 (KHTML, like Gecko) Version/9.0.1 Safari/601.2.7'}
+    r = requests.get(website,headers=headers)
     soup = BeautifulSoup(r.text,"html.parser")
     contents = soup.find("div", {id_or_class: element})
 
