89b357b177b8d6023fa9f448fcc0b130c53ffe0f
Carlos Correia <carlos.correia@ongagement.com>
2016-07-14 02:26:22 +0200

    developing websiteMonitor

--- core_features/tasks.py
+++ core_features/tasks.py
@@ -6,8 +6,7 @@ import requests
 from bs4 import BeautifulSoup
 from django.db.models import ObjectDoesNotExist
 
-
-website = 'https://service.berlin.de/terminvereinbarung/termin/tag.php?id=&buergerID&buergername=&absagecode=&Datum=1467324000&anliegen[]=120335&dienstleister[]=327316&dienstleister[]=327312&dienstleister[]=327314&dienstleister[]=327346&dienstleister[]=327423&dienstleister[]=327348&dienstleister[]=122252&dienstleister[]=327338&dienstleister[]=122260&dienstleister[]=327340&dienstleister[]=122262&dienstleister[]=122254&dienstleister[]=327278&dienstleister[]=327274&dienstleister[]=327276&dienstleister[]=327294&dienstleister[]=327290&dienstleister[]=327292&dienstleister[]=122291&dienstleister[]=327270&dienstleister[]=122285&dienstleister[]=327266&dienstleister[]=122286&dienstleister[]=327264&dienstleister[]=122296&dienstleister[]=327268&dienstleister[]=150230&dienstleister[]=327282&dienstleister[]=327286&dienstleister[]=327284&dienstleister[]=122312&dienstleister[]=122314&dienstleister[]=122304&dienstleister[]=327330&dienstleister[]=122311&dienstleister[]=327334&dienstleister[]=122309&dienstleister[]=327332&dienstleister[]=317869&dienstleister[]=325341&dienstleister[]=324434&dienstleister[]=327352&dienstleister[]=122283&dienstleister[]=327354&dienstleister[]=122276&dienstleister[]=327324&dienstleister[]=122274&dienstleister[]=327326&dienstleister[]=122267&dienstleister[]=327328&dienstleister[]=122246&dienstleister[]=327318&dienstleister[]=122251&dienstleister[]=327320&dienstleister[]=122257&dienstleister[]=327322&dienstleister[]=122208&dienstleister[]=327298&dienstleister[]=122226&dienstleister[]=327300&herkunft=http://service.berlin.de/dienstleistung/120335/standort/327346/'
+website = 'https://goo.gl/0XMmXn'
 element = 'calendar-table'
 id_or_class = 'class'
 
